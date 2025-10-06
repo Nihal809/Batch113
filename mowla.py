7a4cb76a581e044d1c6371570df100ff88079994
Nihal809 <mahmudulhassan6789@gmail.com>
2025-10-06 19:55:46 +0600

    first commit

---
+++ mowla.py
@@ -0,0 +1,9 @@
+a= 60
+b=30 
+
+print(a+b)
+
+
+# def my_f(x,y):
+#     return x+y
+
---
+++ nihal.py
@@ -0,0 +1,78 @@
+# a="I love bangladesh"
+# print(a)
+# i= 1
+# for i in range(10):
+#     print("I love bangladesh")
+
+# def rahim(a,b):
+#     return a+b
+
+
+# x= rahim(5,7)
+
+# print(x)
+
+# 6th floor ,,, 15500, 31000,,, 
+
+
+# a= open("E:/python/new project/nihal nnnnn.txt","a")
+# print(a.write("you busterd"))
+
+# b= open("E:/python/new project/nihal nnnnn.txt","r")
+# print(b.read())
+
+# a.close()
+
+
+# try:
+#     a= int(input("enter a number:"))
+    
+#     for i in range(1,11):
+#         print(f"{a}x{i}={a*i}")
+
+# except Exception as e:
+#     print(e)
+
+    
+# b= "i love bangladesh"
+# print(b)
+# print(len(b))
+# print(b.upper())
+# print(b.lower()) 
+
+
+
+
+# import turtle
+# turtle.speed(6)
+# turtle.color("red")
+# turtle.pensize(2)
+# turtle.bgcolor("light blue")
+
+# i=0 
+# while i<72:
+#     for _ in range(4):
+#         turtle.forward(100)
+#         turtle.left(90)
+
+#     turtle.left(5)
+#     i=i+1
+
+# turtle.exitonclink
+
+
+# a= int(input("Enter a number:"))
+# for i in range(1,11):
+#     print(f"{i}x{a}={i*a}")
+
+class A:
+    x= 12 
+    y= 23
+
+    def _init_(self, name,age):
+        
+        self.a= name
+        self.b= age 
+my_class= A("rahim", 23)
+print(my_class.a)
+
